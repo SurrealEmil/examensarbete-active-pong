ef4ccbc8d2d1030da2c7b314898d8b11b6e6454c
SurrealEmil <emil.ejderklev1@hotmail.com>
2025-03-21 10:57:18 +0100

    Small fix for apiconfig

--- src/web.client/src/config/apiConfig.js
+++ src/web.client/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:7070/api";
+const API_BASE_URL = "http://localhost:5124/api";
 
 axios.defaults.withCredentials = true; // Always send cookies with requests
 
