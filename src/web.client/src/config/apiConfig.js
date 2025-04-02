ad7214b3d29e2150b3b90c100b0311a04c96fdff
SurrealEmil <emil.ejderklev1@hotmail.com>
2025-04-02 21:59:49 +0200

    Right url
    In frontend change API_BASE_URL into new and correct

--- src/web.client/src/config/apiConfig.js
+++ src/web.client/src/config/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:5124/api";
-const API_BASE_URL = "https://activedemopong-fndke6cfdbavhmbq.swedencentral-01.azurewebsites.net/api";
+const API_BASE_URL = "https://activepong.azurewebsites.net/api";
 
 axios.defaults.withCredentials = true; // Always send cookies with requests
 
