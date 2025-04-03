2f8e33cf7f6e089340ada813f782d8de191f2b09
SurrealEmil <emil.ejderklev1@hotmail.com>
2025-04-03 12:53:54 +0200

    Replace broken QR code package with react-qr-code for Vite compatibility

--- src/web.client/src/components/StartScreen/StartScreen.jsx
+++ src/web.client/src/components/StartScreen/StartScreen.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { QRCode } from "react-qrcode-logo";
+import QRCode from "react-qr-code";
 import './StartScreen.css';
 import PongBackground from './PongBackground';
 
