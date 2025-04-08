21985761ae733bdc540eaebd6135ffb76c609584
robinruden <145325673+robinruden@users.noreply.github.com>
2025-04-08 16:06:05 +0200

    styling

--- src/web.client/src/components/PongGame/PongGame-Tournament.jsx
+++ src/web.client/src/components/PongGame/PongGame-Tournament.jsx
@@ -569,7 +569,7 @@ const { fps, isLagSpike } = useGameLoop({
 
 
 // Timer state: starts at 90 seconds
-const [timer, setTimer] = useState(40);
+const [timer, setTimer] = useState(400 );
 /* const [showLeaderboard, setShowLeaderboard] = useState(false);
  */
 
