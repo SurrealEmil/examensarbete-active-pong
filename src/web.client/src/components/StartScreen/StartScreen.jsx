d8803eaa7fcf15869fc52f7d7456edeea68e4eeb
Robin Rudén <robinruden@gmail.com>
2025-05-05 13:22:06 +0200

    updated styling

--- src/web.client/src/components/StartScreen/StartScreen.jsx
+++ src/web.client/src/components/StartScreen/StartScreen.jsx
@@ -311,7 +311,7 @@ const StartScreen = () => {
       {selectedMode ? (
         <div className="old-mode-info-container">
             <div className="qr-code">
-              <QRCode value="https://activepong.azurewebsites.net/signup" size={170} />
+              <QRCode value="https://activepong.azurewebsites.net/signup" size={1} />
             </div>
             <div className="startscreen-text">
               <p>Register and scan your QR code to jump into the action
